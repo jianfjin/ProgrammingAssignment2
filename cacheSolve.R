8e5385e63f64a46618aeae197e121eccc3e5859f
Jianfeng Jin <jianfjin@gmail.com>
2014-12-20 16:06:32 +0100

    update the comments

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,11 +1,18 @@
 cacheSolve <- function(x, ...) {
-    i <- x$getinverse()
-    if (!is.null(i)) {
-        message("getting cached data")
+        ## This function returns the inverse matrix of object x from cache
+  
+        ## return the value of inverse matrix of object x if it is in cache
+        i <- x$getinverse()
+        if (!is.null(i)) {
+            message("getting cached data")
+            i
+        }
+        ## if the inverse matrix is not in cache
+        ## get the value of object x and store it in data
+        data <- x$get()
+        ## set the value of inverse matrix using solve()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        ## return the inverse matrix
         i
-    }
-    data <- x$get()
-    i <- solve(data, ...)
-    x$setinverse(i)
-    i
 }
--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,11 +1,26 @@
 makeCacheMatrix <- function(x = matrix()) {
-    i <- NULL
-    set <- function(y) {
-        x <<- y
-        i <<- NULL
-    }
-    get <- function() x
-    setinverse <- function(inverse = solve(x)) i <<- inverse
-    getinverse <- function() i
-    list(set =set, get = get, setinverse = setinverse, getinverse = getinverse)
+        ## This function calculates the inverse of input matrix
+        ## and returns a list of of functions which
+        ## set the value of input matrix
+        ## get the value of input matrix
+        ## set the value of inverse matrix
+        ## get the value of inverse matrix
+  
+        i <- NULL
+        ## set the value of input matrix
+        ## store the matrix in cache
+        set <- function(y) {
+            x <<- y
+            i <<- NULL
+        }
+        ## get the value of input matrix
+        get <- function() x
+        ## set the vlaue of inverse matrix
+        ## the inverse matrix is calculated by solve()
+        ## store the matrix in cache
+        setinverse <- function(inverse = solve(x)) i <<- inverse
+        ## get the value of inverse matrix
+        getinverse <- function() i
+        ## return the list of functions
+        list(set =set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
